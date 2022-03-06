bd5b7ff75b5c0afb1449241554fe063affb82e43
eliezer <eliezer.moraes@outlook.com>
2022-03-06 07:12:45 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/eliezermoraes/workshopmongodb/WorkshopmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.eliezermoraes.workshopmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eliezermoraes/workshopmongodb/WorkshopmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliezermoraes.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
