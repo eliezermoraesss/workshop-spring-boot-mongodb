e9932ad6f03fec4dc88b35801e34731ae2d7ca2f
eliezermoraesss <eliezer.moraes@outlook.com>
2022-08-14 08:19:25 -0300

    Referenciando os posts do usuario

--- src/main/java/com/eliezermoraes/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/eliezermoraes/workshopmongodb/config/Instantiation.java
@@ -41,6 +41,9 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/18"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 
 	}
 }
