788f0171dadd17b10c1a3ac9bfa54bf4d1917d86
eliezermoraesss <eliezer.moraes@outlook.com>
2022-08-15 11:23:45 -0300

    Consulta simples com @Query

--- src/main/java/com/eliezermoraes/workshopmongodb/services/PostService.java
+++ src/main/java/com/eliezermoraes/workshopmongodb/services/PostService.java
@@ -22,6 +22,6 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text) {
-		return repository.findByTitleContainingIgnoreCase(text);
+		return repository.searchTitle(text);
 	}
 }
